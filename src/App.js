a9b2989ec4f552006f67d2555ac9f29fde48d128
cineminnam <cine.minnam@gmail.com>
2020-03-30 16:37:25 +0900

    #2 JSX & Props
    #2 JSX & Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+
+const memILike = [{
+  id:1,
+  name: "조승연",
+  rating: 5
+},
+{
+  id:2,
+  name: "조유리",
+  rating: 4
+},
+{
+  id:3,
+  name: "김민주",
+  rating: 3
+},
+{
+  id:4,
+  name: "장원영",
+  rating: 3
+},
+{
+  id:5,
+  name: "김채원",
+  rating: 3
+},
+]
+
+function Member({fav, rating}){
+return (
+<div>
+  <h1>I love {fav}</h1>
+  <h4>{rating} / 5</h4>
+</div>);
+}
+
+Member.propTypes = {
+  fav: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {memILike.map(mem=>(
+        <Member key={mem.id} fav={mem.name} rating={mem.rating}/>
+      ))}
     </div>
   );
 }
