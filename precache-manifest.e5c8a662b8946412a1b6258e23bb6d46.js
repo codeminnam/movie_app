5231f459e21351441dcd76e82972f23a48619f1d
cineminnam <cine.minnam@gmail.com>
2020-03-31 11:32:31 +0900

    Updates

---
+++ precache-manifest.e5c8a662b8946412a1b6258e23bb6d46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c960adbcd572247dbb6de343db81133",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0596f4a819eb242212fd",
+    "url": "/movie_app/static/css/main.8bf5ebb4.chunk.css"
+  },
+  {
+    "revision": "697e48abbd7f0ba75f99",
+    "url": "/movie_app/static/js/2.75f33243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.75f33243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0596f4a819eb242212fd",
+    "url": "/movie_app/static/js/main.3adcee84.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
